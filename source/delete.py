675cb1900cb3391f6e9671aaa037350158b2dd8c
MarcScott <marc@coding2learn.org>
2015-03-26 14:08:45 +0000

    added a clock

---
+++ source/delete.py
@@ -0,0 +1,11 @@
+bottles = 10
+while bottles > 0:
+    print(str(bottles) + ' green bottles sitting on the wall')
+    print(str(bottles) + ' green bottles sitting on the wall')
+    print('And if one green bottle should accidently fall')
+    bottles = bottles -1
+    print("There'll be " + str(bottles) + ' green bottles, sitting on the wall')
+    
+
+
+    
